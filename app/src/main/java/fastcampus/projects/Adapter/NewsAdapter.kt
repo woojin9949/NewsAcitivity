7499a9274cba3200065fec3431da03a5b3781ed5
woojin <woojin9949@gmail.com>
2023-08-02 17:08:35 +0900

    .

--- app/src/main/java/fastcampus/projects/Adapter/NewsAdapter.kt
+++ app/src/main/java/fastcampus/projects/Adapter/NewsAdapter.kt
@@ -6,16 +6,20 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import fastcampus.projects.model.NewsItem
 import fastcampus.projects.model.NewsModel
 import fastcampus.projects.newsacitivity.databinding.ItemNewsBinding
 
-class NewsAdapter : ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil) {
+class NewsAdapter(private val onClick: (String) -> Unit) :
+    ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil) {
 
     inner class ViewHolder(private val binding: ItemNewsBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: NewsModel) {
             binding.titleTextView.text = item.title
+            binding.root.setOnClickListener {
+                onClick(item.link)
+            }
+
             Glide.with(binding.thumbnailImageView)
                 .load(item.imageUrl)
                 .into(binding.thumbnailImageView)
--- app/src/main/java/fastcampus/projects/newsacitivity/MainActivity.kt
+++ app/src/main/java/fastcampus/projects/newsacitivity/MainActivity.kt
@@ -1,8 +1,13 @@
 package fastcampus.projects.newsacitivity
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
@@ -21,14 +26,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var newsAdapter: NewsAdapter
-    private var retrofit = Retrofit.Builder()
-        .baseUrl("https://news.google.com/")
-        .addConverterFactory(
+    private var retrofit =
+        Retrofit.Builder().baseUrl("https://news.google.com/").addConverterFactory(
             TikXmlConverterFactory.create(
                 //모든 정보를 다 안가져와도 Exception이 안뜨게 설정!!
-                TikXml.Builder()
-                    .exceptionOnUnreadXml(false)
-                    .build()
+                TikXml.Builder().exceptionOnUnreadXml(false).build()
             )
         ).build()
 
@@ -37,15 +39,86 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        newsAdapter = NewsAdapter()
+        newsAdapter = NewsAdapter { url ->
+            startActivity(Intent(this, WebViewActivity::class.java).apply {
+                putExtra("url", url)
+            })
+        }
+
+        val newsService = retrofit.create(NewsService::class.java)
 
         binding.newsRecyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = newsAdapter
         }
 
-        val newsService = retrofit.create(NewsService::class.java)
-        newsService.mainFeed().enqueue(object : Callback<NewsRss> {
+        binding.feedChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.feedChip.isChecked = true
+            newsService.mainFeed().submitList()
+        }
+
+        binding.politicsChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.politicsChip.isChecked = true
+
+            newsService.politicsNews().submitList()
+        }
+        binding.economyChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.economyChip.isChecked = true
+
+            newsService.economyNews().submitList()
+        }
+        binding.societyChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.societyChip.isChecked = true
+
+            newsService.societyNews().submitList()
+        }
+
+        binding.sportsChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.sportsChip.isChecked = true
+
+            newsService.sportsNews().submitList()
+        }
+        binding.scienceChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.scienceChip.isChecked = true
+
+            newsService.itNews().submitList()
+        }
+        binding.healthChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.healthChip.isChecked = true
+
+            newsService.healthNews().submitList()
+        }
+        binding.searchTextInputEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                binding.chipGroup.clearCheck()
+
+                binding.searchTextInputEditText.clearFocus()
+
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+
+                newsService.search(binding.searchTextInputEditText.text.toString()).submitList()
+
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+
+        }
+
+        binding.feedChip.isChecked = true
+        newsService.mainFeed().submitList()
+    }
+
+    //재사용을 위해 Call<NewsRss>식으로
+    private fun Call<NewsRss>.submitList() {
+        this.enqueue(object : Callback<NewsRss> {
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
 
                 //jsoup을 통해 해당 링크에 헤더에 있는 (meta태그)의 property가  og:title과 og:image 정보를 가져올 수 있다
@@ -53,16 +126,26 @@ class MainActivity : AppCompatActivity() {
                     //response로 받는 객체들(NewsItem)을 NewsModel객체로 transform (NewsModel에 썼음)
                     val list = response.body()?.channel?.items.orEmpty().transform()
                     newsAdapter.submitList(list)
+
+                    //검색했을 시에 해당 검색어 관련 기사 리스트에 없을 시에 애니메이션 실행 visibility
+                    binding.notFoundAnimationView.isVisible = list.isEmpty()
                     //링크 잘 나옴
-                    list.forEach {
-                        Log.d("Testt", "" + it.link)
+                    list.forEachIndexed { index, news ->
                         Thread {
-                            val jsoup = Jsoup.connect(it.link).get()
-                            val elements = jsoup.select("meta[property^=og:]")
-                            val ogImageNode = elements.find { node ->
-                                node.attr("property") == "og:image"
+                            try {
+                                val jsoup = Jsoup.connect(news.link).get()
+                                val elements = jsoup.select("meta[property^=og:]")
+                                val ogImageNode = elements.find { node ->
+                                    node.attr("property") == "og:image"
+                                }
+                                news.imageUrl = ogImageNode?.attr("content")
+                            } catch (e: Exception) {
+                                e.printStackTrace()
                             }
-                            it.imageUrl = ogImageNode?.attr("content")
+                            //메인 쓰레드에선 불가 하므로 runOnUiThread
+                            runOnUiThread {
+                                newsAdapter.notifyItemChanged(index)
+                            }
                         }.start()
                     }
 
@@ -74,4 +157,5 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
 }
